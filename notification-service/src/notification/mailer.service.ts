d4837b3ab07182a8188002d85948f08065e36196
Tarik Kaldzija <tkaldzija@gmail.com>
2023-01-18 15:51:15 +0100

    added sendgrid mailer

--- notification-service/src/notification/mailer.service.ts
+++ notification-service/src/notification/mailer.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import * as sgMail from '@sendgrid/mail';
+import { ConfigService } from '../../config/config.service';
+
+type MailData = Omit<sgMail.MailDataRequired, 'from'>;
 
 @Injectable()
 export class MailerService {
-  constructor() {}
+  constructor(private readonly configService: ConfigService) {
+    sgMail.setApiKey(this.configService.get().SENDGRID_API_KEY);
+  }
 
-  async send(data) {
-    return true;
+  async send(data: MailData) {
+    const mailData = {
+      ...data,
+      from: this.configService.get().SENDGRID_VERIFIED_SENDER,
+    } as sgMail.MailDataRequired;
+    try {
+      await sgMail.send(mailData);
+    } catch (e) {
+      console.log('Error while sending email to', data.to);
+
+      throw e;
+    }
   }
 }
